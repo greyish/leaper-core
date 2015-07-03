816572f9071d08141bce223942dd8e782a3ae9a4
Vinoth <a.vinothk@outlook.com>
2015-07-03 23:13:29 +0530

    cleanup messy code from hackathon

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,17 +18,4 @@ class User < ActiveRecord::Base
   has_many :employees
   has_many :teams
   has_many :leave_types
-
-  def update_employees
-    slack_url = "https://slack.com/api/users.list?token=#{self.slack_api_token}"
-    resp = HTTParty.get(slack_url).as_json
-    unless resp["ok"]
-      Rails.logger.info "Error Fetching URL"
-      return
-    end
-
-    resp["members"].each do |emp|
-      self.employees.create(:name => emp["real_name"], :slack_id => emp["id"], :slack_name => emp["name"], :email => emp["profile"]["email"])
-    end
-  end
 end
--- lib/leave_request_process.rb
+++
@@ -1,6 +0,0 @@
-module LeaveRequestProcess
-
-  def leave_request_create(params)
-    @employee.leave_requests.create(params)
-  end
-end
